fba8bd40311aecf3c060784c9dcc0a0d5b7a7cb9
Joao Teixeira <joao@e-guana.com.br>
2018-07-12 11:45:36 -0300

    Teste 1

--- test-server.js
+++ test-server.js
@@ -7,7 +7,7 @@ app.use(express.static('public'));
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
 
-    res.send("Hello!!");
+    res.send("Hello 2!!");
 });
 
 app.listen(8080, function () {
